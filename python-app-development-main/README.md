316243c7757a0eaa672c4d54dc4e21ea1bd0d5ec
tharan00 <richcuisine7777@gmail.com>
2026-03-05 14:21:01 +0530

    First commit

---
+++ python-app-development-main/README.md
@@ -0,0 +1,2 @@
+# python-app-development
+learning python CI/CD
---
+++ python-app-development-main/app2.py
@@ -0,0 +1,8 @@
+print("Hello from Python app!")
+
+# Simple test to make sure it works
+def main():
+    print("Application is running!")
+    
+if __name__ == "__main__":
+    main()
